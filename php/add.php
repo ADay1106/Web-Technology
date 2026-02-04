d22cf3fa545fc4656024ad4d345922b2e0437d37
Aditya Raj <adityaayush6407@gmail.com>
2026-02-04 16:02:20 +0530

    Add files via upload

---
+++ php/add.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Add Student</title>
+</head>
+<body>
+
+<h2>Add Student</h2>
+
+<form method="post">
+    Name: <input type="text" name="name" required><br><br>
+    Marks: <input type="number" name="marks" required><br><br>
+    <input type="submit" name="submit" value="Add Student">
+</form>
+
+</body>
+</html>
+
+<?php
+include "db.php";
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $marks = $_POST['marks'];
+
+    $sql = "INSERT INTO students (name, marks) VALUES ('$name', '$marks')";
+    mysqli_query($conn, $sql);
+
+    echo "Student added successfully";
+}
+?>
